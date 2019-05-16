861081e92be6680f8ad880a5be2f7cbc33518aee
mpadge <mark.padgham@email.com>
2019-05-16 10:55:55 +0200

    export magrittr::pipe

--- R/moveability-package.R
+++ R/moveability-package.R
@@ -8,3 +8,13 @@
 #' @importFrom RcppParallel RcppParallelLibs
 #' @useDynLib moveability, .registration = TRUE
 NULL
+
+#' Pipe operator
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
