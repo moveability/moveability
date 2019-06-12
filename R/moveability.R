8817175e8ac2e87a2751fcd96572cfc9585c8c12
mpadge <mark.padgham@email.com>
2019-06-12 12:52:29 +0200

    :bathtub:main moveability fn

--- R/moveability.R
+++ R/moveability.R
@@ -20,77 +20,107 @@ moveability <- function (streetnet = NULL, city = NULL, d_threshold = 1,
     if (is.null (city) & is.null (streetnet))
         stop ("city or streetnet must be provided")
     if (!is.null (city) & !is.null (streetnet))
+    {
         message ("City will be ignored, as streetnet has been provided")
+        city <- NULL
+    }
 
     if (!is.null (city))
     {
-        # nocov start
-        streetnet <- osmdata::opq (city) %>%
-            osmdata::add_osm_feature (key = "highway") %>%
-            osmdata::osmdata_sc (quiet = FALSE)
-        # nocov end
+        streetnet <- dl_streetnet (city) # nocov
     } else if (!(methods::is (streetnet, "osmdata_sc") |
                methods::is (streetnet, "dodgr_streetnet_sc")))
         stop ("streetnet must be of format osmdata_sc, or dodgr_streetnet_sc")
        
+    obj <- construct_moveability_objects (streetnet, mode, quiet)
+
+    obj$verts$m <- move_stats (obj$net, from = obj$from, quiet = quiet)
+    return (obj$verts)
+}
+
+dl_streetnet <- function (city)
+{
+    # nocov start
+    osmdata::opq (city) %>%
+        osmdata::add_osm_feature (key = "highway") %>%
+        osmdata::osmdata_sc (quiet = FALSE)
+    # nocov end
+}
+
+# The primary objects are the contracted street network, the table of junction
+# vertices, and the names of central vertices to be used for routing. Note that
+# the inclusion of turn penalties for mode = "bicycle" means that the `from`
+# vertices include potential "_start" suffixes, whereas names in the `verts`
+# table do not include these. Thus, the explicit `from` object must also be
+# constructed and passed to `move_stats`
+construct_moveability_objects <- function (streetnet, mode, quiet)
+{
     if (!methods::is (streetnet, "dodgr_streetnet_sc"))
     { # then convert to dodgr fmt
-        if (!mode %in% c ("foot", "bicycle"))
-            stop ("mode must be either foot or bicycle")
-        if (!quiet)
-        {
-            message ("contracting street network ... ", appendLF = FALSE)
-            pt0 <- proc.time ()
-        }
-
-        dodgr::dodgr_cache_off ()
-        if (mode == "foot")
-        {
-            streetnet <- dodgr::weight_streetnet (streetnet,
-                                                  wt_profile = mode)
-            netc <- dodgr::dodgr_contract_graph (streetnet)
-            verts <- dodgr::dodgr_vertices (netc)
-            from <- verts$id
-        } else # bicycle
-        {
-            streetnet_t <- dodgr::weight_streetnet (streetnet,
-                                                    wt_profile = "bicycle",
-                                                    turn_penalty = TRUE)
-            streetnet <- dodgr::weight_streetnet (streetnet,
-                                                  wt_profile = "bicycle",
-                                                  turn_penalty = FALSE)
-
-            # select vertices from `turn_penalty = F`, then re-map them onto
-            # graph with turn penalties:
-            streetnet_c <- dodgr::dodgr_contract_graph (streetnet)
-            verts <- dodgr::dodgr_vertices (streetnet_c)
-
-            v0 <- gsub ("_start", "",
-                        streetnet_t$.vx0 [grep ("_start", streetnet_t$.vx0)])
-            from <- verts$id
-            from [from %in% v0] <- paste0 (from [from %in% v0], "_start")
-
-            netc <- dodgr::dodgr_contract_graph (streetnet_t, verts = from)
-
-            # some vertices may nevertheless only end up as destination vertices
-            # in contracted graph, so:
-            index <- which (from %in% netc$.vx0)
-            from <- from [index]
-            verts <- verts [index, ]
-        }
-        if (!quiet)
-        {
-            pt <- paste0 (round ((proc.time () - pt0) [3]))
-            message (paste ("done in", pt, "seconds."))
-        }
+        res <- convert_streetnet (streetnet, mode, quiet)
     } else
     {
         netc <- dodgr::dodgr_contract_graph (streetnet)
         from <- unique (netc$.vx0)
         verts <- dodgr::dodgr_vertices (netc)
         verts <- verts [which (verts$id %in% from), ]
+
+        res <- list (net = netc, from = from, verts = verts)
     }
 
-    verts$m <- move_stats (netc, from = from, quiet = quiet)
-    return (verts)
+    return (res)
+}
+
+convert_streetnet <- function (streetnet, mode, quiet)
+{
+    if (!mode %in% c ("foot", "bicycle"))
+        stop ("mode must be either foot or bicycle")
+    if (!quiet)
+    {
+        message ("contracting street network ... ", appendLF = FALSE)
+        pt0 <- proc.time ()
+    }
+
+    dodgr::dodgr_cache_off ()
+    if (mode == "foot")
+    {
+        streetnet <- dodgr::weight_streetnet (streetnet,
+                                              wt_profile = mode)
+        netc <- dodgr::dodgr_contract_graph (streetnet)
+        verts <- dodgr::dodgr_vertices (netc)
+        from <- verts$id
+    } else # bicycle
+    {
+        streetnet_t <- dodgr::weight_streetnet (streetnet,
+                                                wt_profile = "bicycle",
+                                                turn_penalty = TRUE)
+        streetnet <- dodgr::weight_streetnet (streetnet,
+                                              wt_profile = "bicycle",
+                                              turn_penalty = FALSE)
+
+        # select vertices from `turn_penalty = F`, then re-map them onto
+        # graph with turn penalties:
+        streetnet_c <- dodgr::dodgr_contract_graph (streetnet)
+        verts <- dodgr::dodgr_vertices (streetnet_c)
+
+        v0 <- gsub ("_start", "",
+                    streetnet_t$.vx0 [grep ("_start", streetnet_t$.vx0)])
+        from <- verts$id
+        from [from %in% v0] <- paste0 (from [from %in% v0], "_start")
+
+        netc <- dodgr::dodgr_contract_graph (streetnet_t, verts = from)
+
+        # some vertices may nevertheless only end up as destination vertices
+        # in contracted graph, so:
+        index <- which (from %in% netc$.vx0)
+        from <- from [index]
+        verts <- verts [index, ]
+    }
+    if (!quiet)
+    {
+        pt <- paste0 (round ((proc.time () - pt0) [3]))
+        message (paste ("done in", pt, "seconds."))
+    }
+    
+    list (net = netc, from = from, verts = verts)
 }
