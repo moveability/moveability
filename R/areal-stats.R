5a587af62415b10991a416de93a780d46c2d8530
mpadge <mark.padgham@email.com>
2019-06-13 21:29:16 +0200

    extend get-attractors

--- R/areal-stats.R
+++ R/areal-stats.R
@@ -59,6 +59,35 @@ get_attractors <- function (city, quiet = FALSE)
                 "parking_space", "taxi", "ticket_validator")
     key <- rep ("amenity", length (value))
     transportation <- get_one_kv (city, key = key, value = value, quiet = quiet)
+
+    value <- c ("baby_hatch", "clinic", "dentist", "doctors", "hospital",
+                "nursing_home", "pharmacy", "social_facility", "veterinary")
+    key <- rep ("amenity", length (value))
+    healthcare <- get_one_kv (city, key = key, value = value, quiet = quiet)
+
+    value <- c ("arts_centre", "brothel", "casino", "cinema",
+                "community_centre", "fountain", "gambling", "music_venue",
+                "nightclub", "planetarium", "social_centre", "stripclub",
+                "studio", "swingerclub", "theatre")
+    key <- rep ("amenity", length (value))
+    entertainment <- get_one_kv (city, key = key, value = value, quiet = quiet)
+
+    value <- c ("animal_boarding", "animal_shelter", "baking_oven", "bench",
+                "clock", "courthouse", "coworking_space", "crematorium",
+                "crypt", "dive_centre", "dojo", "embassy", "fire_station",
+                "firepit", "game_feeding", "grave_yard", "gym", "hunting_stand",
+                "internet_cafe", "kitchen", "kneipp_water_cure", "marketplace",
+                "monastery", "photo_booth", "place_of_worship", "police",
+                "post_box", "post_depot", "post_office", "prison",
+                "public_bath", "public_building", "ranger_station", "recycling",
+                "sanitary_dump_station", "sauna", "shelter", "shower", "table",
+                "telephone", "toilets", "townhall", "vending_machine",
+                "waste_basket", "waste_disposal", "waste_transfer_station",
+                "watering_place", "water_point")
+    key <- rep ("amenity", length (value))
+    other <- get_one_kv (city, key = key, value = value, quiet = quiet)
+    # TODO: some of these last ones have alternative non-amenity keys
+    # https://wiki.openstreetmap.org/wiki/Key:amenity
 }
 
 get_one_kv <- function (city, key, value, quiet = quiet)
@@ -80,16 +109,39 @@ get_one_kv <- function (city, key, value, quiet = quiet)
     }
     temp <- do.call (c, temp)
 
-    index <- lapply (keys, function (i) !is.na (temp$osm_points [[i]]))
-    index <- which (index [[1]] | index [[2]])
-    temp$osm_points <- temp$osm_points [index, ]
-    xy_point <- sf::st_coordinates (temp$osm_points)
+    xy_point <- NULL
+    if (!is.null (temp$osm_points) & nrow (temp$osm_points) > 0)
+    {
+        index <- lapply (keys, function (i) !is.na (temp$osm_points [[i]]))
+        if (length (index) > 0)
+        {
+            i <- index [[1]]
+            index [[1]] <- NULL
+            for (j in index)
+                i <- i | j
+            index <- which (i)
+            temp$osm_points <- temp$osm_points [index, ]
+            xy_point <- sf::st_coordinates (temp$osm_points)
+        }
+    }
 
-    index <- lapply (keys, function (i) !is.na (temp$osm_polygons [[i]]))
-    index <- which (index [[1]] | index [[2]])
-    temp$osm_polygons <- temp$osm_polygons [index, ]
-    suppressWarnings (xy_poly <- sf::st_centroid (temp$osm_polygons$geometry) %>%
-        sf::st_coordinates ())
+    xy_poly <- NULL
+    if (!is.null (temp$osm_polygons))
+    {
+        index <- lapply (keys, function (i) !is.na (temp$osm_polygons [[i]]))
+        if (length (index) > 0)
+        {
+            i <- index [[1]]
+            index [[1]] <- NULL
+            for (j in index)
+                i <- i | j
+            index <- which (i)
+            temp$osm_polygons <- temp$osm_polygons [index, ]
+            suppressWarnings (
+                  xy_poly <- sf::st_centroid (temp$osm_polygons$geometry) %>%
+                      sf::st_coordinates ())
+        }
+    }
 
     res <- data.frame (id = c (temp$osm_points$osm_id,
                                temp$osm_polygons$osm_id),
@@ -101,6 +153,8 @@ get_one_kv <- function (city, key, value, quiet = quiet)
                         temp$osm_polygons [[k]])
     res$x = c (xy_point [, 1], xy_poly [, 1])
     res$y = c (xy_point [, 2], xy_poly [, 2])
+
+    if (nrow (res) == 0) res <- NULL
 
     return (res)
 }
